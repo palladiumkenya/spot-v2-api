19882176cdea2f19afee52cfdc511949973adf03
cbrianbet <cbrianbet@outlook.com>
2023-06-23 21:05:19 +0300

    pulling only indicator data needed

--- app/routers/indicators.py
+++ app/routers/indicators.py
@@ -7,16 +7,29 @@ router = APIRouter()
 
 @router.get("/")
 async def get_indicators_current():
-    pipeline = [
-        {'$match': {"is_current": True}},
-    ]
-    indicators = indicatorListEntity(Indicators.aggregate(pipeline))
-    return {'indicators': indicators}
+	pipeline = [
+		{'$match': {"is_current": True}},
+	]
+	indicators = indicatorListEntity(Indicators.aggregate(pipeline))
+	return {'indicators': indicators}
 
 @router.get("/{code}")
 async def get_facility_indicators_current(code: int):
-    pipeline = [
-        {'$match': {"is_current": True, "mfl_code": code}},
-    ]
-    indicators = indicatorListEntity(Indicators.aggregate(pipeline))
-    return {'indicators': indicators}
+	pipeline = [
+		{'$match': {"is_current": True, "mfl_code": code}},
+	]
+	indicators = indicatorListEntity(Indicators.aggregate(pipeline))
+	dwh_values = [item["dwh_value"] for item in indicators]
+	emr_values = [item["emr_value"] for item in indicators]
+	names = [item["name"] for item in indicators]
+	dwh_dates = [item["dwh_indicator_date"] for item in indicators]
+	emr_dates = [item["emr_indicator_date"] for item in indicators]
+	result = {
+		"dwh_values": dwh_values,
+		"emr_values": emr_values,
+		"names": names,
+		"dwh_dates": dwh_dates,
+		"emr_dates": emr_dates
+	}
+
+	return {'indicators': result}
