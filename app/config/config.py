a52d35e4ec23c29dcb475af666e9a154a93a5f5d
cbrianbet <cbrianbet@outlook.com>
2023-07-11 15:27:54 +0300

    updated config

--- app/config/config.py
+++ app/config/config.py
@@ -2,11 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_PASSWORD: str
-    DB_USER: str
     DB: str
-    DB_HOST: str
-    DB_PORT: int
     MONGODB_URL: str
     RABBIT_URL: str
 
