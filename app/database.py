0f9c7a88c6937cd8a964e79d4d3e667f426fa7b5
cbrianbet <cbrianbet@outlook.com>
2023-08-23 16:57:05 +0300

    Streamlined indexes

--- app/database.py
+++ app/database.py
@@ -13,12 +13,16 @@ Facility.create_index([("mfl_code", pymongo.ASCENDING)], unique=True)
 Notices = db.notices
 Notices.create_index([("rank", pymongo.ASCENDING)], unique=False)
 Indicators = db.indicators
-Indicators.create_index([("created_at", pymongo.DESCENDING)], unique=False)
+Indicators.create_index([("is_current", pymongo.DESCENDING),("created_at", pymongo.DESCENDING)], unique=False)
 Dockets = db.dockets
 Dockets.create_index([("name", pymongo.ASCENDING)], unique=True)
 Manifests = db.manifests
+Manifests.create_index([("is_current", pymongo.DESCENDING), ("created_at", pymongo.DESCENDING)])
 Extracts = db.extracts
+Extracts.create_index([("created_at", pymongo.DESCENDING)])
 FacilityMetrics = db.facility_metrics
+FacilityMetrics.create_index([("is_current", pymongo.DESCENDING), ("created_at", pymongo.DESCENDING)])
 Profiles = db.profiles_vw
 Handshake = db.handshake
 Log = db.messages_log
+Error = db.error_log
