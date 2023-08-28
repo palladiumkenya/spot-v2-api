3e2894ee0356985c596ae3d4f87f87b5ce6c31a2
cbrianbet <cbrianbet@outlook.com>
2023-08-28 10:39:26 +0300

    Update cron_script.sh

--- cron_script.sh
+++ cron_script.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# Define the endpoints and their respective schedules
+ENDPOINT1="http://localhost:4141/api/dwh_refresh/"
 
-0 6 * * * curl localhost:4141/api/dwh_refresh/
+# Add the cron jobs
+echo "0 6 * * * curl $ENDPOINT1" >> /etc/cron.d/my-cron
+
+# Apply the cron jobs
+crontab /etc/cron.d/my-cron
+
+# Start cron in the background
+cron -f &
