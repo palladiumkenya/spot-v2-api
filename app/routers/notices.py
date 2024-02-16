115506eb4790d70b16e207adbfd0be240e45b40e
cbrianbet <cbrianbet@outlook.com>
2024-02-16 11:21:34 +0300

    Added dwapi version to notices

--- app/routers/notices.py
+++ app/routers/notices.py
@@ -4,12 +4,13 @@ from fastapi import APIRouter, BackgroundTasks, Depends
 from sqlalchemy.orm import Session
 from app.serializers.noticeSerializer import noticesListEntity
 from app.serializers.facilitySerializer import facilityListEntity
-from app.database import Notices, Profiles, Facility, get_db
+from app.database import Notices, Profiles, Facility, FacilityMetrics, get_db
 from app.api.publish_complete import send_to_rabbitmq
 from app.api.dwh_indicators import fetch_and_post, get_period
-
+from app.serializers.facilityMetricsSerializer import facilityMetricsListEntity
 
 router = APIRouter()
+
 
 @router.get("/")
 async def get_notices():
@@ -23,13 +24,18 @@ async def get_notices():
 @router.get('/update_notices')
 async def start_periodic_task(background_tasks: BackgroundTasks):
     manifest = Profiles.find({})
+    dwapi_metrics = facilityMetricsListEntity(FacilityMetrics.find({"is_current": True, 'metric': 'DWAPI Version'}))
+    dwapi_versions = {metric['mfl_code']: metric['value'] for metric in dwapi_metrics}
 
     for item in manifest:
         if item and item.get("totalExpected") == item.get("totalReceived") == item.get("totalQueued"):
+            dwapi_version = None
+            if item['mfl_code'] in dwapi_versions:
+                dwapi_version = dwapi_versions[item['mfl_code']]
             message = str({"Facility": item['facility'], "MFL Code": item['mfl_code'], "Docket": item['docket'],
                            "indicator_date": get_period()[2].strftime('%Y-%m-%d'), "Message": "complete!",
-                           "log_date": datetime.datetime.now().isoformat()})
-            
+                           "dwapi_version": dwapi_version, "log_date": datetime.datetime.now().isoformat()})
+
             background_tasks.add_task(send_to_rabbitmq, message)
     return {"message": "Started sending notices"}
 
@@ -43,16 +49,36 @@ async def pull_dwh_indicators(background_tasks: BackgroundTasks, db: Session = D
 
     for facility in facilities:
         print(f'starting {facility["mfl_code"]}')
-        
-        background_tasks.add_task(fetch_and_post,
-            db, facility, f"SELECT SUM(Tested) as value FROM Reporting.dbo.AggregateHTSUptake WHERE MFLCode = {facility['mfl_code']} and year = {year} and month = {month}", 'HTS_TESTED')
-        background_tasks.add_task(fetch_and_post,
-            db, facility, f"SELECT SUM(Positive) as value FROM Reporting.dbo.AggregateHTSUptake WHERE MFLCode = {facility['mfl_code']} and year = {year} and month = {month}", 'HTS_TESTED_POS')
-        background_tasks.add_task(fetch_and_post,
-            db, facility, f"SELECT SUM(patients_startedART) as value FROM Reporting.dbo.AggregateTxNew WHERE MFLCode = {facility['mfl_code']} and StartARTYearMonth = '{year}-{month}'", 'TX_NEW')
-        background_tasks.add_task(fetch_and_post,
-            db, facility, f"SELECT SUM(CountClientsTXCur) as value FROM Reporting.dbo.AggregateTXCurr WHERE MFLCode = {facility['mfl_code']}", 'TX_CURR')
-        
+
+        background_tasks.add_task(
+            fetch_and_post,
+            db,
+            facility,
+            f"SELECT SUM(Tested) as value FROM Reporting.dbo.AggregateHTSUptake WHERE MFLCode = {facility['mfl_code']} and year = {year} and month = {month}",
+            'HTS_TESTED'
+        )
+        background_tasks.add_task(
+            fetch_and_post,
+            db,
+            facility,
+            f"SELECT SUM(Positive) as value FROM Reporting.dbo.AggregateHTSUptake WHERE MFLCode = {facility['mfl_code']} and year = {year} and month = {month}",
+            'HTS_TESTED_POS'
+        )
+        background_tasks.add_task(
+            fetch_and_post,
+            db,
+            facility,
+            f"SELECT SUM(patients_startedART) as value FROM Reporting.dbo.AggregateTxNew WHERE MFLCode = {facility['mfl_code']} and StartARTYearMonth = '{year}-{month}'",
+            'TX_NEW'
+        )
+        background_tasks.add_task(
+            fetch_and_post,
+            db,
+            facility,
+            f"SELECT SUM(CountClientsTXCur) as value FROM Reporting.dbo.AggregateTXCurr WHERE MFLCode = {facility['mfl_code']}",
+            'TX_CURR'
+        )
+
         print(f'finishing {facility["mfl_code"]}')
 
     return {"message": "Started sending dwh indicators"}
